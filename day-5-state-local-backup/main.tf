7d7a511737a25efed70110808428b0eccaff54ba
nikita <rathodnikita243@gmail.com>
2026-03-12 22:26:34 +0530

    day-5-terraform project

--- day-5-state-local-backup/main.tf
+++ day-5-state-local-backup/main.tf
@@ -1,16 +1,16 @@
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
-        Name = "terraform"
+        Name = "test"
     }
   
 }
 
 resource "aws_instance" "name" {
-    ami = "ami-0b0b78dcacbab728f"
+    ami = "ami-0f559c3642608c138"
     instance_type = "t2.micro"
     tags = {
-        Name = "terrafom-instance"
+        Name = "sia-instance"
     }   
   
 }
--- day-5-state-remote backend/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-backup-7723"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
-  }
-}
