9c0fbf0ad410edd3bc6e252cd3948da73bba0960
nikita <rathodnikita243@gmail.com>
2026-03-13 22:59:38 +0530

    day-6-statelocking

--- day-6-statelocking/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-s3-lockfile"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-    use_lockfile = true  #dynmodb no longer required for state locking in s3 backend we can use lockfile for state locking in s3 backend
-    #terraform version shouid be 1.10 above to use lockfile for state locking in s3 backend
-  }
-}
--- day-6-statelocking/main.tf
+++ day-6-statelocking/main.tf
@@ -14,12 +14,3 @@ resource "aws_subnet" "name" {
     }
   
 }
-
-resource "aws_subnet" "name-1" {
-    vpc_id = aws_vpc.name.id
-    cidr_block = "10.0.1.0/24"
-    tags = {
-        Name = "prod-subnet"
-    }
-  
-}
