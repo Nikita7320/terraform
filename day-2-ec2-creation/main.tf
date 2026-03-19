9ad506395c889ad753c805cdc61a4f172adae221
nikita <rathodnikita243@gmail.com>
2026-03-19 22:57:44 +0530

    day-2-ec2-creation

--- day-2-ec2-creation/main.tf
+++ day-2-ec2-creation/main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "dev_instance" {
     ami = var.ami_id
     instance_type = var.instance_type
     tags = { 
-      Name = "test_instance"
+      Name = "dev_instance"
     } 
 }
