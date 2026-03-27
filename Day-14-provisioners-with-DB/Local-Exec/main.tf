c2ac6de4f6d752d4df216a459111df29cf10f7ff
nikita <rathodnikita243@gmail.com>
2026-03-27 14:45:37 +0530

    Day-14-provisioners-with-DB

--- Day-14-provisioners-with-DB/Local-Exec/main.tf
+++ Day-14-provisioners-with-DB/Local-Exec/main.tf
@@ -4,6 +4,25 @@ provider "aws" {
 
 
 # Create the RDS instance
+
+resource "aws_security_group" "rds_sg" {
+  name = "rds-sg"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # testing only
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_db_instance" "mysql_rds" {
   identifier              = "my-mysql-db"
   engine                  = "mysql"
@@ -14,18 +33,19 @@ resource "aws_db_instance" "mysql_rds" {
   allocated_storage       = 20
   skip_final_snapshot     = true
   publicly_accessible     = true
-}
 
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+}
 # Use null_resource to execute the SQL script from your local machine
 resource "null_resource" "local_sql_exec" {
+
+  provisioner "local-exec" {
+    command = "mysql -h my-mysql-db.cspyogikkr6m.us-east-1.rds.amazonaws.com -u admin -pPassword123! < init.sql"
+
+    interpreter = ["C:\\Program Files\\Git\\bin\\bash.exe", "-c"]
+  }
 
   triggers = {
     always_run = timestamp()
   }
-provisioner "local-exec" {
-  command = "mysql -h ${aws_db_instance.mysql_rds.address} -u admin -pPassword123! dev < init.sql"
 }
- 
-}
-
-  
