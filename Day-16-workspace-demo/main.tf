2a1e8efb756ad29b407f44216e7e1ef067f50e3c
nikita <rathodnikita243@gmail.com>
2026-03-28 20:23:40 +0530

    Day-16-workspace-demo

---
+++ Day-16-workspace-demo/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-02dfbd4ff395f2a1b" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name        = "${terraform.workspace}-server"
+    Environment = terraform.workspace
+  }
+}
